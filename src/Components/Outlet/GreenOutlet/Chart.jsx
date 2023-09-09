607d384b5664ac33eab160c37f30e20d072cc44c
nipaaa <akternipa941@gmail.com>
2023-09-09 17:07:34 +0600

    update chart

--- src/Components/Outlet/GreenOutlet/Chart.jsx
+++ src/Components/Outlet/GreenOutlet/Chart.jsx
@@ -5,11 +5,9 @@ const Chart = () => {
   const data = [{ name: "Group A", value: 80 }];
   const startAngle = 70;
   return (
-    <PieChart width={800} height={400}>
+    <PieChart width={200} height={200}>
       <Pie
         data={data}
-        cx={120}
-        cy={200}
         innerRadius={60}
         outerRadius={80}
         fill="#F5FAF8"
